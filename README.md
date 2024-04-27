7cc41662b0cdbf460ac103ea83da44522cf0a2a6
lswlc33 <lswlc33@outlook.com>
2024-04-27 21:05:05 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# pyQuickOllama
+
+这是一个能够快速获取 `ollama` 模型列表并打开模型的命令行的工具  
+该工具可以避免频繁的输入 `ollama run xxxx` 和 `ollama list`  
+提高日常使用的流畅度与舒适度
---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+
+model_list = []
+
+
+def run_model(model_name):
+    os.system("cls")
+    os.system(f"ollama run {model_name}")
+
+
+def check_input(str):
+    return 0 <= int(str) - 1 < len(model_list)
+
+
+def get_model_list():
+    f = os.popen("ollama list")
+    data = f.readlines()
+    # remove header
+    for i in range(len(data)):
+        if "NAME" in data[i]:
+            data = data[i + 1 :]
+            break
+    # get name from line
+    for i in range(len(data)):
+        data[i] = data[i].split("\t")[0].strip()
+    f.close()
+    return data
+
+
+if __name__ == "__main__":
+    model_list = get_model_list()
+
+    if not model_list:
+        print("No models available")
+        os.system("pause")
+    else:
+        os.system("cls")
+
+        print("Available models:\n")
+        for i in range(len(model_list)):
+            print(f"{i + 1}. {model_list[i]}")
+
+        print("\nchoose the index to enter")
+        index = input("\nType index here:")
+
+        if check_input(index):
+            run_model(model_list[int(index) - 1])
+        else:
+            print("Invalid input")
+            os.system("pause")
